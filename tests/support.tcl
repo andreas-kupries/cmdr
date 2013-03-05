3cc805bbd80780349d83c70897f8b65199651e2f
Andreas Kupries <akupries@shaw.ca>
2013-03-05 20:49:40 +0000

    Split tests into multiple files, added support code, and changed state dumpers to generate output independent of the exact object instance commands (i.e. no ::oo::ObjXXX anymore).

---
+++ tests/support.tcl
@@ -0,0 +1,112 @@
+# -*- tcl -*-
+# # ## ### ##### ######## ############# #####################
+## Supporting procedures for xo.test et. al.
+
+proc ShowOfficer {o} { Wrap [DumpOfficer $o] }
+proc ShowPrivate {o} { Wrap [DumpPrivate $o] }
+
+# Indent a list of lines, generate text block.
+proc Wrap {list} {
+    set p "\n    "
+    return ${p}[join $list $p]\n
+}
+
+# Dumping the state of an officer and its subordinates.
+# Assumes that the children are all oficers too, and not privates.
+# This may be fixable.
+
+proc DumpOfficer {o} {
+    set name [$o fullname]
+    set result {}
+
+    # Description
+    lappend result "$name = \{"
+    lappend result "    description: [$o description]"
+    # Default action, if any.
+    if {[$o hasdefault]} {
+	lappend result "    default: [$o default]"
+    } else {
+	lappend result "    no default"
+    }
+    # Data store. Note how it shows the entire inherited store, not
+    # just the local settings.
+    foreach k [lsort -dict [$o keys]] {
+	lappend result "    store ($k): '[$o get $k]'"
+    }
+    # Delegates - Action mapping
+    foreach a [lsort -dict [$o known]] {
+	set c [$o lookup $a]
+	lappend result "    $a --> [$c name]"
+    }
+    lappend result "\}"
+
+    # Delegates II. Full dump of the subordinates.
+    set tmp {}
+    foreach c [$o children] {
+	lappend tmp [list $c [$c name]]
+    }
+    foreach item [lsort -dict -index 1 $tmp] {
+	lassign $item c _
+	lappend result {*}[DumpOfficer $c]
+    }
+    return $result
+}
+
+# Dumping the state of a private and its parameters.
+proc DumpPrivate {o} {
+    set name [$o fullname]
+    set result {}
+
+    # Description
+    lappend result "$name = \{"
+    lappend result "    description: [$o description]"
+
+    # Data store. Inherited. Note how it shows the entire inherited
+    # store, not just the local settings. Which a private usually has
+    # none of.
+    foreach k [lsort -dict [$o keys]] {
+	lappend result "    store ($k): '[$o get $k]'"
+    }
+
+    # Options (Mapping from prefix to full options).
+    foreach {opt v} [kt dictsort [$o options]] {
+	lappend result "    $opt --> ($v)"
+	# TODO: option handler and configuration
+    }
+
+    # All parameters.
+    foreach name [lsort -dict [$o names]] {
+	set c [$o lookup $name]
+
+	lappend result "    P ($name) \{"
+	lappend result "        description: [$c description]"
+
+	set state {}
+	if {[$c ordered]}     { lappend state ordered }
+	if {[$c hidden]}      { lappend state hidden }
+	if {[$c list]}        { lappend state splat }
+	if {[$c required]}    { lappend state required }
+	if {[$c interactive]} { lappend state interact }
+	if {[llength $state]} {
+	    lappend result "        [join $state {, }]"
+	}
+
+	if {[$c hasdefault]}  {
+	    lappend result "        default: '[$c default]'"
+	} else {
+	    lappend result "        no default"
+	}
+	if {[$c interactive]} {
+	    lappend result "        prompt: '[$c prompt]'"
+	}
+
+	lappend result "        \[[$c options]\]"
+	lappend result "        g ([$c generator])"
+	lappend result "        v ([$c validator])"
+	lappend result "        o ([$c on])"
+	lappend result "    \}"
+    }
+
+    lappend result "\}"
+    return $result
+}
