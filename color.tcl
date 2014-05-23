e71d78f11bdd2854ba9658151c3c0496d383bc4c
Andreas Kupries <akupries@shaw.ca>
2014-05-23 07:13:21 +0000

    cmdr::color - Fixed issues, expanded functionality, better checking. Plus testsuite.

--- color.tcl
+++ color.tcl
@@ -27,8 +27,14 @@ package require cmdr::tty
 
 # # ## ### ##### ######## ############# #####################
 
+namespace eval ::cmdr {
+    namespace export color
+    namespace ensemble create
+}
+
 namespace eval ::cmdr::color {
-    namespace export activate active define
+    namespace export \
+	activate active names get get-def define exists unset
     namespace ensemble create \
 	-unknown [namespace current]::Unknown
     # Note, the option ensures that all unknown methods are treated as
@@ -48,10 +54,8 @@ debug level  cmdr/color
 debug prefix cmdr/color {[debug caller] | }
 
 # # ## ### ##### ######## ############# #####################
-## TODO undef?
-## TODO multi-def (load)
-## TODO get (display)
-## officer and private for std commands (show, define).
+## TODO multi-def aka bulk-def aka load
+## TODO officer and private for std commands (show, define).
 
 proc ::cmdr::color::activate {{flag 1}} {
     debug.cmdr/color {}
@@ -65,50 +69,114 @@ proc ::cmdr::color::active {} {
     return  $active
 }
 
+proc ::cmdr::color::names {} {
+    debug.cmdr/color {}
+    variable def
+    return [dict keys $def]
+}
+
+proc ::cmdr::color::unset {name} {
+    debug.cmdr/color {}
+    variable def
+    if {![dict exists $def $name]} {
+	return -code error \
+	    -errorcode [list CMDR COLOR BAD $name] \
+	    "Expected a color name, got \"$name\""
+    }
+
+    variable char
+    dict unset def  $name
+    dict unset char $name
+    return
+}
+
+proc ::cmdr::color::get {name} {
+    debug.cmdr/color {}
+    variable def
+    if {![dict exists $def $name]} {
+	return -code error \
+	    -errorcode [list CMDR COLOR BAD $name] \
+	    "Expected a color name, got \"$name\""
+    }
+    variable char
+    return [dict get $char $name]
+}
+
+proc ::cmdr::color::get-def {name} {
+    debug.cmdr/color {}
+    variable def
+    if {![dict exists $def $name]} {
+	return -code error \
+	    -errorcode [list CMDR COLOR BAD $name] \
+	    "Expected a color name, got \"$name\""
+    }
+    return [dict get $def $name]
+}
+
+proc ::cmdr::color::exists {name} {
+    debug.cmdr/color {}
+    variable def
+    return [dict exists $def $name]
+}
+
 proc ::cmdr::color::define {name spec} {
     debug.cmdr/color {}
     variable def
     variable char
-    # TODO: spec may be
-    # => reference to other color name, or
-    # => raw control sequence, or
-    # => RGB spec.
+
+    # The spec may be
+    # (1) A reference to other color name.
+    # (2) An RGB spec.
+    # (3) A raw control sequence.
 
     # Syntax:
-    # ref = anything already found as key in the database.
-    # rgb = 
-    # raw = 
+    # (1) ref := =<name>
+    # (2) rgb := %<r>,<g>,<b>
+    # (3) raw := anything else
 
-    if {[dict exists $def $spec]} {
-	if {$spec eq $name} {
+    if {[regexp {^=(.*)$} $spec -> ref]} {
+	if {$ref eq $name} {
 	    return -code error \
 		-errorcode [list CMDR COLOR CIRCLE $name] \
 		"Rejected self-referential definition of \"$name\""
+	} elseif {![dict exists $def $ref]} {
+	    return -code error \
+		-errorcode [list CMDR COLOR BAD $ref] \
+		"Expected a color name, got \"$ref\""
+	} else {
+	    set raw [dict get $char $ref]
+	    debug.cmdr/color {reference, resolved => [Quote $raw]}
+	    dict set def  $name $spec
+	    dict set char $name $raw
+	    return
 	}
-	debug.cmdr/color {reference, resolved => [S [dict get $char $spec]]}
-	dict set def  $name $spec
-	dict set char $name [dict get $char $spec]
-	return
     }
 
-    if {[regexp {^%(\d+),(\d+),(\d+)$} $spec -> r g b]} {
-	# R, G, B all in range 0..5
-	set r [Clamp $r]
-	set g [Clamp $g]
-	set b [Clamp $b]
-	# 256 mapping
-	# code = 16 + 36R + 6G + B --> [16..236]
-	set code [expr {16 + 36*$r + 6*$g + $b}]
-	debug.cmdr/color {RGB encoded => [S [C $code]]}
-	dict set def  $name $spec
-	dict set char $name [C $code]
-	return
+    if {[regexp {^%(.*)$} $spec -> rgb]} {
+	if {![regexp {^(\d+),(\d+),(\d+)$} $rgb -> r g b]} {
+	    return -code error \
+		-errorcode [list CMDR COLOR BAD-RGB SYNTAX $rgb] \
+		"Expected an RGB tuple, got \"$rgb\""
+	} {
+	    # R, G, B all in range 0..5
+	    Clamp R $r
+	    Clamp G $g
+	    Clamp B $b
+	    # 256-color mapping
+	    # code = 16 + 36R + 6G + B --> [16..236]
+	    set point [expr {16 + 36*$r + 6*$g + $b}]
+	    set code [Code $point]
+	    debug.cmdr/color {RGB encoded => [Quote $code]}
+	    dict set def  $name $spec
+	    dict set char $name $code
+	    return
 
-	# Legacy mapping
-	# R,G,B mapping 0,1 --> 0, 2,3 --> 1, 4,5 --> 2
-	# bold mapping: 0,1,2 --> 0,1,1 (set if any of R, G, B)
-	# code = 8bold + R + 2G + 4B
-	#      = 8, for R==G==B != 0, special case.
+	    # Legacy mapping
+	    # R,G,B mapping 0,1 --> 0, 2,3 --> 1, 4,5 --> 2
+	    # bold mapping: 0,1,2 --> 0,1,1 (set if any of R, G, B)
+	    # code = 8bold + R + 2G + 4B
+	    #      = 8, for R==G==B != 0, special case.
+	}
     }
 
     # Raw control sequence, simply save
@@ -134,7 +202,7 @@ proc ::cmdr::color::Apply {codes text} {
 
     variable char
     foreach c $codes {
-	if {[dict exists $char $c]} {
+	if {![dict exists $char $c]} {
 	    return -code error \
 		-errorcode [list CMDR COLOR UNKNOWN $c] \
 		"Expected a color name, got \"$c\""
@@ -148,20 +216,21 @@ proc ::cmdr::color::Apply {codes text} {
     return $r
 }
 
-proc ::cmdr::color::S {text} {
+proc ::cmdr::color::Quote {text} {
     # quote all non-printable characters (< space, > ~)
     variable smap
     return [string map $smap $text]
 }
 
-proc ::cmdr::color::C {args} {
+proc ::cmdr::color::Code {args} {
     return \033\[[join $args \;]m
 }
 
-proc ::cmdr::color::Clamp {x} {
-    if {$x < 0} { return 0 }
-    if {$x > 5} { return 5 }
-    return $x
+proc ::cmdr::color::Clamp {label x} {
+    if {($x >= 0) && ($x <= 5)} { return $x }
+    return -code error \
+	-errorcode [list CMDR COLOR BAD-RGB RANGE $x] \
+	"RGB channel $label out of range, got $x"
 }
 
 # # ## ### ##### ######## ############# #####################
@@ -180,78 +249,65 @@ namespace eval ::cmdr::color {
     variable char {}
 
     # Colors. Foreground/Text.
-    define  black        [C 30]  ; # Black  
-    define  red          [C 31]  ; # Red    
-    define  green        [C 32]  ; # Green  
-    define  yellow       [C 33]  ; # Yellow 
-    define  blue         [C 34]  ; # Blue   
-    define  magenta      [C 35]  ; # Magenta
-    define  cyan         [C 36]  ; # Cyan   
-    define  white        [C 37]  ; # White  
-    define  default      [C 39]  ; # Default (Black)
+    define  black        [Code 30]  ; # Black  
+    define  red          [Code 31]  ; # Red    
+    define  green        [Code 32]  ; # Green  
+    define  yellow       [Code 33]  ; # Yellow 
+    define  blue         [Code 34]  ; # Blue   
+    define  magenta      [Code 35]  ; # Magenta
+    define  cyan         [Code 36]  ; # Cyan   
+    define  white        [Code 37]  ; # White  
+    define  default      [Code 39]  ; # Default (Black)
 
     # Colors. Background.
-    define  bg-black     [C 40]  ; # Black  
-    define  bg-red       [C 41]  ; # Red    
-    define  bg-green     [C 42]  ; # Green  
-    define  bg-yellow    [C 43]  ; # Yellow 
-    define  bg-blue      [C 44]  ; # Blue   
-    define  bg-magenta   [C 45]  ; # Magenta
-    define  bg-cyan      [C 46]  ; # Cyan   
-    define  bg-white     [C 47]  ; # White  
-    define  bg-default   [C 49]  ; # Default (Transparent)
+    define  bg-black     [Code 40]  ; # Black  
+    define  bg-red       [Code 41]  ; # Red    
+    define  bg-green     [Code 42]  ; # Green  
+    define  bg-yellow    [Code 43]  ; # Yellow 
+    define  bg-blue      [Code 44]  ; # Blue   
+    define  bg-magenta   [Code 45]  ; # Magenta
+    define  bg-cyan      [Code 46]  ; # Cyan   
+    define  bg-white     [Code 47]  ; # White  
+    define  bg-default   [Code 49]  ; # Default (Transparent)
 
     # Non-color attributes. Activation.
-    define  bold         [C  1]  ; # Bold  
-    define  dim          [C  2]  ; # Dim
-    define  italic       [C  3]  ; # Italics      
-    define  underline    [C  4]  ; # Underscore   
-    define  blink        [C  5]  ; # Blink
-    define  revers       [C  7]  ; # Reverse      
-    define  hidden       [C  8]  ; # Hidden
-    define  strike       [C  9]  ; # StrikeThrough
+    define  bold         [Code  1]  ; # Bold  
+    define  dim          [Code  2]  ; # Dim
+    define  italic       [Code  3]  ; # Italics      
+    define  underline    [Code  4]  ; # Underscore   
+    define  blink        [Code  5]  ; # Blink
+    define  revers       [Code  7]  ; # Reverse      
+    define  hidden       [Code  8]  ; # Hidden
+    define  strike       [Code  9]  ; # StrikeThrough
 
     # Non-color attributes. Deactivation.
-    define  no-bold      [C 22]  ; # Bold  
-    define  no-dim       [C __]  ; # Dim
-    define  no-italic    [C 23]  ; # Italics      
-    define  no-underline [C 24]  ; # Underscore   
-    define  no-blink     [C 25]  ; # Blink
-    define  no-revers    [C 27]  ; # Reverse      
-    define  no-hidden    [C 28]  ; # Hidden
-    define  no-strike    [C 29]  ; # StrikeThrough
+    define  no-bold      [Code 22]  ; # Bold  
+    define  no-dim       [Code __]  ; # Dim
+    define  no-italic    [Code 23]  ; # Italics      
+    define  no-underline [Code 24]  ; # Underscore   
+    define  no-blink     [Code 25]  ; # Blink
+    define  no-revers    [Code 27]  ; # Reverse      
+    define  no-hidden    [Code 28]  ; # Hidden
+    define  no-strike    [Code 29]  ; # StrikeThrough
 
     # Remainder
-    define  reset        [C  0]  ; # Reset
+    define  reset        [Code  0]  ; # Reset
 
     # And now the standard symbolic names
-
-    define  confirm red
-    define  error   red
-    define  warning yellow
-    define  note    blue
-    define  good    green
-    define  name    blue
-
-    # header command stopped advisory crashed failure success name prompt table warning
-    # bl/whi bl/yel  bl/grey bl/yel   bl/red  bl/red  bl/gre  bl/cy bl/cy bl/cy bl/mag
-    # stdout - white, stderr - red
-    # app-header sys-header
-    # bl/yel     bl/cy
-
-    # others ...
-    # name	<>	blue,
-    # neutral	<>	blue,
-    # good	<>	green,
-    # bad	<>	red,
-    # error	<>	magenta,
-    # unknown	<>	cyan,
-    # warning	<>	yellow,
-    # instance<>	yellow,
-    # number	<>	green,
-    # prompt	<>	blue,
-    # yes	<>	green,
-    # no	<>	red
+    define  advisory =yellow
+    define  bad      =red
+    define  confirm  =red
+    define  error    =magenta
+    define  good     =green
+    define  name     =blue
+    define  neutral  =blue
+    define  no       =red
+    define  note     =blue
+    define  number   =green
+    define  prompt   =blue
+    define  unknown  =cyan
+    define  warning  =yellow
+    define  yes      =green
 
     variable smap {}
 }
@@ -264,7 +320,7 @@ apply {{} {
 	lappend smap $c $o
     }
     lappend smap \127 \\127
-}} ::cmdr::color
+} ::cmdr::color}
 
 # # ## ### ##### ######## ############# #####################
 ## Ready
